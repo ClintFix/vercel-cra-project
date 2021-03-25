42777cd5ef330140a7d0b7d288723243dcace0d2
ClintFix <clint.fix@gmail.com>
2021-03-25 11:05:30 -0600

    fixed

--- src/App.js
+++ src/App.js
@@ -3,15 +3,7 @@ import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [date, setDate] = useState(null);
-  useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
-    }
-    getDate();
-  }, []);
+  
   return (
     <main>
       <h1>This is Clint's Site</h1>
